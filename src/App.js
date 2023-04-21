5db2bec28b21499d598b7ea72c46e7f0571b12cf
Richard Hoyle <richardhoyle900@gmail.com>
2023-04-21 14:01:13 +0100

    pagination works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
+import Pagination from "./Pagination";
+import axios from "axios";
 
 function App() {
-const [Pokemon, setPokemon] = useState(['bulbasaur', 'charmander'])
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [previousPageUrl, setPreviousPageUrl] = useState();
+
+  useEffect(() => {
+    setIsLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken(c => cancel = c)
+      })
+      .then((res) => {
+        setIsLoading(false);
+        setNextPageUrl(res.data.next);
+        setPreviousPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function goToPreviousPage() {
+    setCurrentPageUrl(previousPageUrl);
+  }
+
+  if (isLoading) return "Rounding up the Pokémon...Hold Tight!";
 
   return (
-    <PokemonList pokemon={Pokemon} />
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        goToNextPage={nextPageUrl ? goToNextPage : null}
+        goToPreviousPage={previousPageUrl ? goToPreviousPage : null}
+      />
+    </>
   );
 }
 
 export default App;
-
-// 
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function Pagination() {
+export default function Pagination({ goToNextPage, goToPreviousPage }) {
   return (
-    <div>Pagination</div>
-  )
+    <div>
+      {goToPreviousPage && <button onClick={goToPreviousPage}>Previous</button>}
+      {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+    </div>
+  );
 }
